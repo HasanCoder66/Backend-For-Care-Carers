d81bceb3d6effde95a09c9b1cee207b410ec034c
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-29 18:08:47 +0500

    working on signup function

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,3 +1,13 @@
-export const signup = async (req, res, next) => {};
+// import UserModel from "../Models/UserModel";
+
+export const signup = async (req, res, next) => {
+
+    // const newUser = new UserModel({
+    //     ...req.body , password 
+    // }) 
+    //// Hashed Password
+    const salt = await bcryptjs.genSalt(15)
+    const hashedPassword = await bcryptjs.hash(req.body.password , salt)
+};
 export const login = async (req, res, next) => {};
 export const forgetPass = async (req, res, next) => {};
--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -3,6 +3,6 @@ import { signup, login, forgetPass } from "../Controllers/authController.js";
 const authRoutes = express.Router();
 
 authRoutes.post("/signup", signup);
-authRoutes.post("/login", login);
-authRoutes.get("/forgetPass", forgetPass);
+// authRoutes.post("/login", login);
+// authRoutes.get("/forgetPass", forgetPass);
 export default authRoutes;
