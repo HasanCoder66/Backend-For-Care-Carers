3d7b7fe72c4e93a3b095d7ede5cc65246db4c0ea
HasanCoder66 <hasanashraf874@gmail.com>
2024-03-29 15:24:43 +0500

    Backend connected

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 const PORT = 8800;
 
+const BackendConnect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("BackEnd Connected");
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
 app.listen(PORT, () => {
+  BackendConnect();
   console.log(`Server listening on this ${PORT}`);
 });
